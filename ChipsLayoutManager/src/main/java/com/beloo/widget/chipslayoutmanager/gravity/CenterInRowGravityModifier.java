5869cda1bce9341147e1e98423d60efa954c82db
alex <a.korchagin@growapp.biz>
2018-06-01 07:18:27 +0300

    removed throwing Exception in CenterInRowGravityModifier

--- ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/gravity/CenterInRowGravityModifier.java
+++ ChipsLayoutManager/src/main/java/com/beloo/widget/chipslayoutmanager/gravity/CenterInRowGravityModifier.java
@@ -2,17 +2,25 @@ package com.beloo.widget.chipslayoutmanager.gravity;
 
 import android.graphics.Rect;
 
+import com.beloo.widget.chipslayoutmanager.util.log.Log;
+
 class CenterInRowGravityModifier implements IGravityModifier {
+    private static final String TAG = "CenterInRowGravityModifier";
+
     @Override
     public Rect modifyChildRect(int minStart, int maxEnd, Rect childRect) {
+        childRect = new Rect(childRect);
+
         if (childRect.top < minStart) {
-            throw new IllegalArgumentException("top point of input rect can't be lower than minTop");
+            Log.e(TAG, "top point of input rect can't be lower than minTop");
+            return childRect;
+            //throw new IllegalArgumentException("top point of input rect can't be lower than minTop");
         }
         if (childRect.bottom > maxEnd) {
             throw new IllegalArgumentException("bottom point of input rect can't be bigger than maxTop");
         }
 
-        childRect = new Rect(childRect);
+
 
         int placeHeight = maxEnd - minStart;
         int rectHeight = childRect.bottom - childRect.top;
